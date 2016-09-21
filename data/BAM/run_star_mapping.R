cb3153fbd62f16bb3714b41f56ba41fad75cf03c
markrobinsonuzh <mark.robinson@imls.uzh.ch>
2016-09-21 14:12:08 +0200

    adjust metadata, add .gitignores

--- data/BAM/run_star_mapping.R
+++ data/BAM/run_star_mapping.R
@@ -6,13 +6,13 @@ names(fq) <- gsub("_[12].fq.gz","",basename(fq))
 
 fqs <- split(fq, names(fq))
 
-inddir <- "/home/mark/projects/teaching/compgen_demo/data/indices/STAR_index_100"
+inddir <- "~/projects/teaching/compgen_rnaseq_2016/data/indices/STAR_index_100"
 
 for(i in 1:length(fqs)) {
   out <- paste0( names(fqs)[i], "_STAR.bam" )
   outs <- gsub(".bam","_s",out)
   cmd <- paste0("STAR --genomeDir ", inddir, " --readFilesIn ", fqs[[i]][1], " ", fqs[[i]][2],
-                " --runThreadN 4 --outFilterMultimapNmax 1 --readFilesCommand zcat; samtools view -Sb Aligned.out.sam > ",
+                " --runThreadN 4 --outFilterMultimapNmax 1 --readFilesCommand gzcat; samtools view -Sb Aligned.out.sam > ",
                 out, " && rm Aligned.out.sam; mv Log.final.out ", out, ".log")
   cat(cmd, "\n")
   st <- system.time(system(cmd))
